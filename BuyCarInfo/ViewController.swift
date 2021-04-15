c52469f62edb3450d69d52c512145acc66f0acd0
Dennis <dennis60506@yahoo.com.tw>
2021-04-15 21:52:39 +0800

    Code Init

---
+++ BuyCarInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BuyCarInfo
+//
+//  Created by Dennis Lin on 2021/4/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
